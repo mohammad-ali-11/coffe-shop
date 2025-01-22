1c513af4a8b9430463c4f45f46400d0a32790b9c
mohammad-ali-11 <mohammadalikhosravi78@gmail.com>
2025-01-22 03:09:11 -0800

    Added Tailwind CSS build and updated project structure

---
+++ docs/scripts/app.js
@@ -0,0 +1,64 @@
+const toggleThemeBtn = document.querySelectorAll(".toggle-theme");
+const submenuOpenBtn=document.querySelector('.submenu-open-btn')
+const navOpenBtn=document.querySelector('.nav-icon')
+const navCloseBtn=document.querySelector('.nav-Close-Btn')
+const nav=document.querySelector('.nav')
+const overlay=document.querySelector('.overlay')
+const navShopping=document.querySelector('.nav-shopping')
+const navOpenShopping=document.querySelector('.navShoppin-icon')
+const navShoppingCloseBtn=document.querySelector('.navShopping-Close-Btn')
+
+
+
+const submenu=document.querySelector('.submenu')
+
+toggleThemeBtn.forEach(btn=>{
+btn.addEventListener("click",function () {
+    if (localStorage.theme === "dark"){
+        document.documentElement.classList.remove("dark");
+        localStorage.theme = "light";
+    } else {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme" , "dark");
+    }   
+})
+})
+
+submenuOpenBtn.addEventListener("click",(e)=>{
+    e.currentTarget.parentElement.classList.toggle('text-orange-200');
+   e. currentTarget.classList.toggle('rotate-180')
+    
+submenu.classList.toggle("submenu--open")
+})
+
+
+function closeNav() {
+    nav.classList.remove("right-0")
+    nav.classList.add("-right-64")
+    overlay.classList.remove("overlay--visible")
+}
+function closeCart() {
+    navShopping.classList.remove("left-0")
+    navShopping.classList.add("-left-64")
+    overlay.classList.remove("overlay--visible")
+   
+}
+
+navOpenBtn.addEventListener('click',()=>{
+nav.classList.remove("-right-64")
+nav.classList.add("right-0")
+overlay.classList.add("overlay--visible")
+overlay.addEventListener('click',closeNav)
+})
+
+navOpenShopping.addEventListener('click',()=>{
+navShopping.classList.remove("-left-64")
+navShopping.classList.add("left-0")
+overlay.classList.add("overlay--visible")
+overlay.addEventListener('click',closeCart)
+})
+
+
+navCloseBtn.addEventListener('click',closeNav)
+navShoppingCloseBtn.addEventListener('click',closeCart)    
+
